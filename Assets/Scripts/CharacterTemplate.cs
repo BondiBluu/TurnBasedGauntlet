2b10dcf9f78a07e6e555fbea7f569ff623b48bd0
Bami O. <bamideleogundeji949@gmail.com>
2024-05-10 13:54:45 -0400

    feat: Add max and current stats to CharacterTemplate
    This commit adds the `max` and `current` fields for various stats to the `CharacterTemplate` class. These fields include `maxHP`, `currentHP`, `maxMP`, `currentMP`, `maxAttack`, `currentAttack`, `maxDefense`, `currentDefense`, `maxSpeed`, `currentSpeed`, `maxMagic`, `currentMagic`, `maxResistance`, `currentResistance`, `maxSkill`, `currentSkill`, `maxEfficiency`, and `currentEfficiency`. This allows for better tracking and management of character stats in the game.

--- Assets/Scripts/CharacterTemplate.cs
+++ Assets/Scripts/CharacterTemplate.cs
@@ -7,6 +7,25 @@ public class CharacterTemplate : MonoBehaviour
     public CharacterData characterData;
     bool isDowned;
 
+    public float maxHP;
+    public float currentHP;
+    public float maxMP;
+    public float currentMP;
+    public float maxAttack;
+    public float currentAttack;
+    public float maxDefense;
+    public float currentDefense;
+    public float maxSpeed;
+    public float currentSpeed;
+    public float maxMagic;
+    public float currentMagic;
+    public float maxResistance;
+    public float currentResistance;
+    public float maxSkill;
+    public float currentSkill;
+    public float maxEfficiency;
+    public float currentEfficiency;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/StatsList.cs
+++ Assets/Scripts/StatsList.cs
@@ -24,23 +24,14 @@ public class StatsList : ScriptableObject
     [TextArea(15, 20)]
     [SerializeField] string abilityDesc;
     [SerializeField] float baseHP;
-    [SerializeField] float currentHP;
     [SerializeField] float baseMP;
-    [SerializeField] float currentMP;
     [SerializeField] float baseAttack;
-    [SerializeField] float currentAttack;
     [SerializeField] float baseDefense;
-    [SerializeField] float currentDefense;
     [SerializeField] float baseSpeed;
-    [SerializeField] float currentSpeed;
     [SerializeField] float baseMagic;
-    [SerializeField] float currentMagic;
     [SerializeField] float baseResistance;
-    [SerializeField] float currentResistance;
     [SerializeField] float baseSkill;
-    [SerializeField] float currentSkill;
     [SerializeField] float baseEfficiency;
-    [SerializeField] float currentEfficiency;
     [SerializeField] Immunity[] immunities;
 
     public string CharacterName { get => characterName; set => characterName = value; }
@@ -48,23 +39,14 @@ public class StatsList : ScriptableObject
     public string CharacterAbility { get => characterAbility; set => characterAbility = value; }
     public string AbilityDesc { get => abilityDesc; set => abilityDesc = value; }
     public float BaseHP { get => baseHP; set => baseHP = value; }
-    public float CurrentHP { get => currentHP; set => currentHP = value; }
     public float BaseMP { get => baseMP; set => baseMP = value; }
-    public float CurrentMP { get => currentMP; set => currentMP = value; }
     public float BaseAttack { get => baseAttack; set => baseAttack = value; }
-    public float CurrentAttack { get => currentAttack; set => currentAttack = value; }
     public float BaseDefense { get => baseDefense; set => baseDefense = value; }
-    public float CurrentDefense { get => currentDefense; set => currentDefense = value; }
     public float BaseSpeed { get => baseSpeed; set => baseSpeed = value; }
-    public float CurrentSpeed { get => currentSpeed; set => currentSpeed = value; }
     public float BaseMagic { get => baseMagic; set => baseMagic = value; }
-    public float CurrentMagic { get => currentMagic; set => currentMagic = value; }
     public float BaseResistance { get => baseResistance; set => baseResistance = value; }
-    public float CurrentResistance { get => currentResistance; set => currentResistance = value; }
     public float BaseSkill { get => baseSkill; set => baseSkill = value; }
-    public float CurrentSkill { get => currentSkill; set => currentSkill = value; }
     public float BaseEfficiency { get => baseEfficiency; set => baseEfficiency = value; }
-    public float CurrentEfficiency { get => currentEfficiency; set => currentEfficiency = value; }
     public Immunity[] Immunities { get => immunities; set => immunities = value; }
 
 
