8b45059ec668c11fca634de56f45d6bbdf9625ae
Bami O. <bamideleogundeji949@gmail.com>
2024-05-16 14:30:54 -0400

    feat: Add panel functionality to ButtonController
    This commit adds panel functionality to the ButtonController class. It introduces methods for showing and hiding different panels based on button clicks, such as OnAttackPanel, OnItemPanel, OnStatsPanel, OnLogPanel, OnQuitPanel, OnAllyPanel, OnEnemyPanel, and OnTurnOffPanels. These changes enhance the user interface by allowing the display of relevant panels and improving the overall user experience.

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -5,20 +5,94 @@ using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
+    [Header("GameObjects")]
+    public GameObject atkPanel;
+    public GameObject itemPanel;
+    public GameObject statsPanel;
+    public GameObject logPanel;
+    public GameObject quitPanel;
+    public GameObject allyPanel;
+    public GameObject enemyPanel;
+    public GameObject blockerPanel;
+    [Header("Buttons")]
     public Button atkButton;
     public Button itemButton;
     public Button statsButton;
     public Button logButton;
     public Button quitButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public void OnAttackPanel(){
+        atkPanel.SetActive(true);
+        blockerPanel.SetActive(true);
+
+        itemPanel.SetActive(false);
+        statsPanel.SetActive(false);
+        logPanel.SetActive(false);
+        quitPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnItemPanel(){
+        itemPanel.SetActive(true);
+        blockerPanel.SetActive(true);
+
+        atkPanel.SetActive(false);
+        statsPanel.SetActive(false);
+        logPanel.SetActive(false);
+        quitPanel.SetActive(false);
+    }
+
+    public void OnStatsPanel(){
+        statsPanel.SetActive(true);
+        blockerPanel.SetActive(true);
+
+        atkPanel.SetActive(false);
+        itemPanel.SetActive(false);
+        logPanel.SetActive(false);
+        quitPanel.SetActive(false);
+    }
+
+    public void OnLogPanel(){
+        logPanel.SetActive(true);
+        blockerPanel.SetActive(true);
+
+        atkPanel.SetActive(false);
+        itemPanel.SetActive(false);
+        statsPanel.SetActive(false);
+        quitPanel.SetActive(false);
+    }
+
+    public void OnQuitPanel(){
+        quitPanel.SetActive(true);
+        blockerPanel.SetActive(true);
+
+        atkPanel.SetActive(false);
+        itemPanel.SetActive(false);
+        statsPanel.SetActive(false);
+        logPanel.SetActive(false);
+    }
+
+    public void OnAllyPanel(){
+        allyPanel.SetActive(true);
+        blockerPanel.SetActive(true);
+
+        enemyPanel.SetActive(false);
+    }
+
+    public void OnEnemyPanel(){
+        enemyPanel.SetActive(true);
+        blockerPanel.SetActive(true);
+
+        allyPanel.SetActive(false);
+    }
+
+    public void OnTurnOffPanels(){
+        atkPanel.SetActive(false);
+        itemPanel.SetActive(false);
+        statsPanel.SetActive(false);
+        logPanel.SetActive(false);
+        quitPanel.SetActive(false);
+        allyPanel.SetActive(false);
+        enemyPanel.SetActive(false);
+        blockerPanel.SetActive(false);
     }
 }
