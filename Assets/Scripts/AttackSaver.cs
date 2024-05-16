60695c47ee4e1ca79488883881ea25e272d2240c
Bami O. <bamideleogundeji949@gmail.com>
2024-05-16 13:51:47 -0400

    feat: Add save functions for moves and items
    This commit adds save functions for moves and items in the AttackSaver class. The `SaveMove` function saves the move used by a character, while the `SaveItem` function saves the item used by a character. These changes improve the functionality of the game by allowing the tracking of character actions in battle.

--- Assets/Scripts/AttackSaver.cs
+++ Assets/Scripts/AttackSaver.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class AttackSaver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public List<SaveActions> characterActionsContainer = new List<SaveActions>();
+
+    //save the move used by the character 
+    public void SaveMove(CharacterTemplate user, Moves move, CharacterTemplate target){
+        characterActionsContainer.Add(new SaveActions(user, move, target));
+        Debug.Log($"Actions in the container: {characterActionsContainer.Count}");
+        Debug.Log($"Move saved. {user.characterData.CharaStatList.CharacterName} used {move.MoveName} on {target.characterData.CharaStatList.CharacterName}");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //save the item used by the character
+    public void SaveItem(CharacterTemplate user, ItemObject item, CharacterTemplate target){
+        characterActionsContainer.Add(new SaveActions(user, item, target));
+        Debug.Log($"Actions in the container: {characterActionsContainer.Count}");
+        Debug.Log($"Item saved. {user.characterData.CharaStatList.CharacterName} used {item.ItemName} on {target.characterData.CharaStatList.CharacterName}");
+    }
+
+    //class that saves the actions of the characters in battle
+    public class SaveActions{
+        public Moves move;
+        public ItemObject item;
+        public CharacterTemplate user;
+        public CharacterTemplate target;
+
+        //constructor for saving moves
+        public SaveActions(CharacterTemplate _user, Moves _move, CharacterTemplate _target){
+            user = _user;
+            move = _move;
+            target = _target;
+        }
+
+        //constructor for saving items
+        public SaveActions(CharacterTemplate _user, ItemObject _item, CharacterTemplate _target){
+            user = _user;
+            item = _item;
+            target = _target;
+        }
     }
 }
+
+
