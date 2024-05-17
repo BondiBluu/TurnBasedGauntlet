6d7713c3b5137c369745caff40dfe98610d92743
Bami O. <bamideleogundeji949@gmail.com>
2024-05-17 13:44:42 -0400

    feat: Refactor BattleSystem PlayerTurn method
    This commit refactors the PlayerTurn method in the BattleSystem class. It removes commented out code and adds a wait condition for the player to select a move and a target. This change improves the flow of the game by ensuring that the player's turn is properly executed and allowing for strategic decision-making during battles.

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -27,6 +27,9 @@ public class BattleSystem : MonoBehaviour
     public Transform[] enemyBattleStations;
     [SerializeField] int currentSeed;
     [SerializeField] int currentGroupSet;
+
+    [SerializeField] CharacterTemplate savedCharacter;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -63,6 +66,35 @@ public class BattleSystem : MonoBehaviour
         StartCoroutine(PlayerTurn());
     }
 
+    public IEnumerator PlayerTurn()
+    {
+        buttonController.atkButton.Select();
+        Debug.Log("Battle Start. Player Turn");
+
+        int currentCharacter = 0;
+
+        for(int i = currentCharacter; i < partyManager.currentParty.Count; i++)
+        {
+            //if the character is downed, skip to the next character
+            // if(partyManager.currentParty[i].currentHP > 0)
+            // {
+            //     currentCharacter = i;
+            //     savedCharacter = partyManager.currentParty[currentCharacter];
+            // } 
+            // else {
+            //     continue;
+            // }
+            
+            currentCharacter = i;
+            savedCharacter = partyManager.currentParty[currentCharacter];
+
+           //wait until the player has selected a move, and a target
+           yield return new WaitUntil(() => currentCharacter == 2);
+           
+        }
+        yield return new WaitForSeconds(1f);
+    }
+
     public void Setup(int groupCount, Transform[] battleStations, List <CharacterTemplate> charaTemplate){
         
         for(int i = 0; i < groupCount; i++)
@@ -77,9 +109,8 @@ public class BattleSystem : MonoBehaviour
         }
     }
 
-    public IEnumerator PlayerTurn()
-    {
-        buttonController.atkButton.Select();
-        yield return new WaitForSeconds(1f);
+    //set up move generation for current character    
+    public void OnChosenAttackButton(){
+        generator.GenerateMoves(savedCharacter);
     }
 }
