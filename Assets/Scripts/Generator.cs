ca172472a5c900429e9c7e40e7dfdd178a030444
Bami O. <bamideleogundeji949@gmail.com>
2024-05-21 14:17:59 -0400

    Refactor Generator class to use selectedMove and selectedItem variables
    This commit refactors the Generator class in the Assets/Scripts/Generator.cs file. The changes include:
    - Renaming the savedMove variable to selectedMove to better represent its purpose
    - Renaming the savedItem variable to selectedItem to better represent its purpose
    
    These changes improve the clarity and readability of the code in the Generator class by using more descriptive variable names.

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -12,8 +12,8 @@ public class Generator : MonoBehaviour
     public Transform moveContainer;
     public Transform itemContainer;
     public float buttonSpacing;
-    public Moves savedMove;
-    public ItemObject savedItem;
+    public Moves selectedMove;
+    public ItemObject selectedItem;
     public List<Moves> movesAlreadyAdded = new List<Moves>();
     ButtonController buttonCon;
 
@@ -34,7 +34,12 @@ public class Generator : MonoBehaviour
 
         float currentPosY = 0f;
 
-        foreach(Moves move in character.characterData.Moveset.Moves){
+
+        //getting each move in movelist
+        for(int i = 0; i < character.characterData.Moveset.Moves.Count; i++){
+
+            Moves move = character.characterData.Moveset.Moves[i];
+            
             if(character.currentLevel >= move.LvlGotten && !movesAlreadyAdded.Contains(move)){
                 movesAlreadyAdded.Add(move);
                 
@@ -74,6 +79,47 @@ public class Generator : MonoBehaviour
                 buttonComp.onClick.AddListener(() => panelAction());
             }
         }
+
+        // foreach(Moves move in character.characterData.Moveset.Moves){
+        //     if(character.currentLevel >= move.LvlGotten && !movesAlreadyAdded.Contains(move)){
+        //         movesAlreadyAdded.Add(move);
+                
+        //         GameObject button = Instantiate(buttonPrefab, moveContainer);
+
+        //         //we use these a lot so we store them in variables
+        //         RectTransform buttonRect = button.GetComponent<RectTransform>();
+        //         Button buttonComp = button.GetComponent<Button>();
+        //         TMP_Text buttonText = button.GetComponentInChildren<TMP_Text>();
+
+        //         //give space between buttons
+        //         buttonRect.anchoredPosition = new Vector2(0, -currentPosY);
+        //         currentPosY += buttonSpacing + buttonRect.sizeDelta.y;
+
+        //         buttonText.text = move.MoveName;
+        //         buttonComp.onClick.AddListener(() => SaveMove(move));
+
+        //         //representing the method that will be called when the button is clicked
+        //         UnityAction panelAction;
+
+        //         switch(move.MovesType){
+        //             case Moves.MoveType.Damaging:
+        //             case Moves.MoveType.Drain:
+        //                 panelAction = buttonCon.OnEnemyPanel;
+        //                 break;
+        //             case Moves.MoveType.Healing:
+        //                 panelAction = buttonCon.OnAllyPanel;
+        //                 break;
+        //             case Moves.MoveType.Supplementary:
+        //             //if the move has debuffs, the panel action will be to open the enemy panel, if it has buffs, the panel action will be to open the ally panel
+        //                 panelAction = move.Debuffs.Length > 0 ? buttonCon.OnEnemyPanel : move.Buffs.Length > 0 ? buttonCon.OnAllyPanel : null;
+        //                 break;
+        //             default:
+        //                 continue;
+        //         }
+
+        //         buttonComp.onClick.AddListener(() => panelAction());
+        //     }
+        // }
     }
 
     public void GenerateItems(){
@@ -89,11 +135,14 @@ public class Generator : MonoBehaviour
     }
 
         public void SaveMove(Moves move){
-        savedMove = move;
+        selectedMove = move;
+        selectedItem = null;
+        
         }
 
         public void SaveItem(ItemObject item){
-        savedItem = item;
+        selectedItem = item;
+        selectedMove = null;
         }
 }
 
