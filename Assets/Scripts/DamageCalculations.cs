dee900d72f0041105e13f5120f6db6e3f2b2f435
Bami O. <bamideleogundeji949@gmail.com>
2024-05-23 12:22:47 -0400

    Refactor DamageCalculations class to improve move and tool calculations
    This commit refactors the DamageCalculations class in the Assets/Scripts/DamageCalculations.cs file. The changes include:
    - Adding new methods to calculate damaging moves, drain moves, tools, potions, healing moves, and MP loss
    - Updating the existing CalcMove method to handle physical and magical attacks
    - Adding logic to handle damage calculation, healing calculation, and MP loss calculation
    - Ensuring that damage and healing values are never less than 1
    
    These changes enhance the functionality of the DamageCalculations class by providing more accurate calculations for moves, tools, potions, and healing, improving the overall gameplay experience.

--- Assets/Scripts/DamageCalculations.cs
+++ Assets/Scripts/DamageCalculations.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class DamageCalculations : MonoBehaviour
@@ -10,9 +12,80 @@ public class DamageCalculations : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public int CalculateDamagingMove(CharacterTemplate user, Moves move, CharacterTemplate target){
+        return CalcMove(user, move, target);
+    }
+
+    public (int damage, int healing) CalculateDrainMove(CharacterTemplate user, Moves move, CharacterTemplate target){
+        int damage = CalcMove(user, move, target);
+        int healing = damage / 2;
+        return (damage, healing);
+    }
+
+    public int CalcMove(CharacterTemplate user, Moves move, CharacterTemplate target){
+        int damage = 0;
+
+        switch(move.AttackingType){
+            case Moves.AttackType.Physical:
+                damage = (int)Math.Ceiling(user.currentAttack * move.MovePower / (2 * Mathf.Max(1, target.currentDefense)));
+                break;
+            case Moves.AttackType.Magical:
+                damage = (int)Math.Ceiling(user.currentMagic * move.MovePower / (2 * Mathf.Max(1, target.currentResistance)));
+                break;
+        }
+
+        //if damage is less than 1, set it to 1
+        if(damage < 1){
+            damage = 1;
+        }
+
+        return damage;
+    }
+
+    public int CalcTool(CharacterTemplate user, ItemObject item, CharacterTemplate target){
+        int damage = 0;
+
+        ToolObject tool = (ToolObject)item;
+
+        if(tool.AtkPwr > 0){
+            damage = (int)Math.Ceiling(tool.AtkPwr + (.15f * user.currentEfficiency));
+        } else if(tool.MagPwr > 0){
+            damage = (int)Math.Ceiling(tool.MagPwr + (.15f * user.currentEfficiency));
+        }
+
+        //if damage is less than 1, set it to 1
+        if(damage < 1){
+            damage = 1;
+        }
+
+        return damage;
+    }
+
+    public (int hp, int mp) CalcPotions(CharacterTemplate user, ItemObject item, CharacterTemplate target){
+        int hp = 0;
+        int mp = 0;
+
+        RestorativeObject potion = (RestorativeObject)item;
+
+        if(potion.HpRestore > 0){
+            hp = (int)Math.Ceiling(potion.HpRestore + (.15f * user.currentEfficiency));
+        }
+
+        if(potion.MpRestore > 0){
+            mp = (int)Math.Ceiling(potion.MpRestore + (.15f * user.currentEfficiency));
+        }
         
+        return (hp, mp);
+    }
+
+    public int CalcHealingMove(CharacterTemplate user, Moves move){
+        //TODO: add healing move calculations. Talk with Elle
+        int healing = 0;
+        return healing;
+    }
+
+    public int CalcMPLoss(Moves move){
+        int mpLoss = (int)move.MPCost;
+        return mpLoss;
     }
 }
