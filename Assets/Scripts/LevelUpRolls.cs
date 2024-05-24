b660c54ec3bf850cd6bb48dc384ec01737e4e343
Bami O. <bamideleogundeji949@gmail.com>
2024-05-24 11:51:16 -0400

    Refactor LevelUpRolls class to handle leveling up based on type
    This commit refactors the LevelUpRolls class in the Assets/Scripts/LevelUpRolls.cs file. The changes include:
    - Adding the LevelRoll method to handle leveling up based on the character's type
    - Implementing the LevelTank method to roll for HP, MP, Attack, Defense, Magic, Resistance, Skill, and Efficiency stats for tank characters
    - Updating the LevelRoll method to call the appropriate leveling up method based on the character's type
    
    These changes enhance the LevelUpRolls class by providing a way to level up characters based on their type, improving the overall gameplay experience.

---
+++ Assets/Scripts/LevelUpRolls.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpRolls : MonoBehaviour
+{
+    //leveling up based on type
+    public int LevelRoll(CharacterTemplate character){
+        int levelRoll = 0;
+        //if there is only one leveluptype, character level rolls twice
+        if(character.characterData.CharaStatList.LevelType.Length == 1){
+            for(int i = 0; i < 2; i++){
+                switch(character.characterData.CharaStatList.LevelType[0]){
+                    case StatsList.LevelUpType.PhysicalTank:
+                        LevelTank(character);
+                        break;
+                }
+            }
+        }
+
+        return levelRoll;
+
+    }
+
+    //To Note: will gather all stat rolls and roll them all at once
+
+    //leveling up based on type: may be int instead of void
+    public void LevelTank(CharacterTemplate character){
+        //roll for HP
+        int hpRoll = Random.Range(2, 7);
+        //roll for MP
+        int mpRoll = Random.Range(0, 2);
+        //roll for Attack
+        int atkRoll = Random.Range(1, 6);
+        //roll for Defense
+        int defRoll = Random.Range(1, 6);
+        //roll for Magic
+        int magRoll = Random.Range(1, 6);
+        //roll for Resistance
+        int resRoll = Random.Range(1, 6);
+        //roll for Skill
+        int skillRoll = Random.Range(1, 6);
+        //roll for Efficiency
+        int effRoll = Random.Range(1, 6);
+
+
+        //add the rolls to the character's stats
+        character.maxHP += hpRoll;
+        character.maxMP += mpRoll;
+        character.maxAttack += atkRoll;
+        character.maxDefense += defRoll;
+        character.maxMagic += magRoll;
+        character.maxResistance += resRoll;
+        character.maxSkill += skillRoll;
+        character.maxEfficiency += effRoll;
+    }
+
+    public void LevelDPS(CharacterTemplate character){
+
+    }
+}
