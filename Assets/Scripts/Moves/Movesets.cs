9ee8039576b13eb8efaf1007078eed94bb4143f6
Bami O. <bamideleogundeji949@gmail.com>
2024-05-29 14:15:58 -0400

    Refactor Movesets class to update menu name for new moveset
    This commit updates the Movesets class in the Assets/Scripts/Moves/Movesets.cs file to change the menu name for creating a new moveset. The previous menu name was "Moves/New Moveset" and it has been updated to "Moves/Moveset/New Moveset". This change improves the organization and clarity of the menu structure for movesets.

--- Assets/Scripts/Moves/Movesets.cs
+++ Assets/Scripts/Moves/Movesets.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Moveset", menuName = "Moves/New Moveset")]
+[CreateAssetMenu(fileName = "Moveset", menuName = "Moves/Moveset/New Moveset")]
 public class Movesets : ScriptableObject
 {
     [SerializeField] List<Moves> moves = new List<Moves>();
