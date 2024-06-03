be05722c99f428c572a603b63dc35168bc9574d6
Bami O. <bamideleogundeji949@gmail.com>
2024-06-03 12:09:33 -0400

    Refactor BattleSystem class to prioritize healing actions

--- Assets/Scripts/DamageCalculations.cs
+++ Assets/Scripts/DamageCalculations.cs
@@ -8,8 +8,10 @@ using UnityEngine;
 public class DamageCalculations : MonoBehaviour
 {
     UIManager uiManager;
+    Generator generator;
     void Start(){
         uiManager = FindObjectOfType<UIManager>();
+        generator = FindObjectOfType<Generator>();
     }
 
     //calculating damaging moves
@@ -95,6 +97,9 @@ public class DamageCalculations : MonoBehaviour
     //putting everything together for the attack 
     public IEnumerator OnAttack(CharacterTemplate user, Moves move, CharacterTemplate target){
 
+        string targetName = target.characterData.CharaStatList.CharacterName;
+        string userName = user.characterData.CharaStatList.CharacterName;
+
         //calc the mp loss first and update the user mp bar
         if(user.characterType == CharacterTemplate.CharacterType.Friendly){
             user.TakeMP(CalcMPLoss(move));
@@ -109,7 +114,8 @@ public class DamageCalculations : MonoBehaviour
         //target pain animation plays
         //target takes damage, but if the move is drain, the target heals as well
         (int damage, int healing) = CalcDamagingMove(user, damagingMove, target);
-        
+
+        string message = $"{userName} used {move.MoveName} on {targetName}. {targetName} lost {damage} hp.";
       
         //update the attacker health bar, if draining move, update the target and user health bars
         target.TakeDamage(damage);
@@ -117,7 +123,9 @@ public class DamageCalculations : MonoBehaviour
         //if healing is greater than 0, heal the user
         if(healing > 0){
             user.HealDamage(healing, 0);
+            message += $" {userName} healed for {healing} hp.";
         }
+
         //update the user health bar
         if(user.characterType == CharacterTemplate.CharacterType.Friendly){
             yield return StartCoroutine(uiManager.UpdateHP(user));
@@ -140,20 +148,33 @@ public class DamageCalculations : MonoBehaviour
         //debuff application if any
         if(damagingMove.Debuffs.Length > 0){
             target.ApplyDebuff(damagingMove.Debuffs, damagingMove.DebuffValue);
+            //using string join to loop through the debuffs and add them to the message
+            message += $" {targetName}'s {string.Join(", ", damagingMove.Debuffs)} lowered.";            
         }       
 
         //TODO: add status to the target if any
+
+        Debug.Log(message);
+        generator.AddToBattleLog(message);
 
         //seconds pass
     }
 
     public IEnumerator OnAttack(CharacterTemplate user, ItemObject item, CharacterTemplate target){
         ToolObject tool = (ToolObject)item;
+
+        string targetName = target.characterData.CharaStatList.CharacterName;
+        string userName = user.characterData.CharaStatList.CharacterName;
+
         //user used this
         //user anim plays
         //target pain anim plays
         //damage to target
-        target.TakeDamage(CalcTool(user, tool, target));
+        int damage = CalcTool(user, tool, target); //used to get the message for the log easier
+        target.TakeDamage(damage);
+
+        string message = $"{userName} used {item.ItemName} on {targetName}. {targetName} lost {damage} hp.";
+        
         //update the target health bar
         if(target.characterType == CharacterTemplate.CharacterType.Friendly){
             yield return StartCoroutine(uiManager.UpdateHP(target));
@@ -165,13 +186,22 @@ public class DamageCalculations : MonoBehaviour
         //debuff application if any
         if(tool.Debuffs.Length > 0){
             target.ApplyDebuff(tool.Debuffs.Cast<DamagingMoves.Debuff>().ToArray(), tool.DebuffAmount);
+            message += $" {targetName}'s {string.Join(", ", tool.Debuffs)} lowered.";            
+
         }
 
         //status effects, if any
+
+        Debug.Log(message);
+        generator.AddToBattleLog(message);
     }
 
     //only player heals
     public IEnumerator OnHeal(CharacterTemplate user, Moves move, CharacterTemplate target){
+
+        string userName = user.characterData.CharaStatList.CharacterName;
+        string targetName = target.characterData.CharaStatList.CharacterName;
+        
         //mp loss and mp bar update
         user.TakeMP(CalcMPLoss(move));
         yield return StartCoroutine(uiManager.UpdateMP(user));
@@ -187,6 +217,9 @@ public class DamageCalculations : MonoBehaviour
         } 
         //target heals
         int healing = CalcHealingMove(user, healingMove, target);
+
+        string message = $"{userName} used {move.MoveName} on {targetName}. {targetName} healed for {healing} hp.";
+
         target.HealDamage(healing, 0);
         //update the target health bar
         yield return StartCoroutine(uiManager.UpdateHP(target));
@@ -195,15 +228,22 @@ public class DamageCalculations : MonoBehaviour
         //reverts debuffs if any
         if(healingMove.Debuffs.Length > 0){
             target.RemoveDebuffs(healingMove.Debuffs);
+            message += $" {targetName}'s  {string.Join(", ", healingMove.Debuffs)}  back to normal.";
         }
 
         //cure status, if status is matches the cure status
 
     
         //seconds pass
+        Debug.Log(message);
+        generator.AddToBattleLog(message);
     }
 
     public IEnumerator OnPotion(CharacterTemplate user, ItemObject item, CharacterTemplate target){
+
+        string userName = user.characterData.CharaStatList.CharacterName;
+        string targetName = target.characterData.CharaStatList.CharacterName;
+
         //item object will now be restorative object
         RestorativeObject potion = (RestorativeObject)item;
         yield return new WaitForSeconds(1f);
@@ -212,6 +252,8 @@ public class DamageCalculations : MonoBehaviour
         //target healed (?)anim plays
         //target heals
         (int hp, int mp) = CalcPotions(user, potion, target);
+
+        string message = $"{userName} used {item.ItemName} on {targetName}. Restored {hp} hp and {mp} mp.";
         target.HealDamage(hp, mp);
         
         //update the target health bar
@@ -220,6 +262,12 @@ public class DamageCalculations : MonoBehaviour
         yield return StartCoroutine(uiManager.UpdateMP(target));
         uiManager.UpdateMPPanel(target);
 
+        //cure debuffs if any
+        if(potion.HealDebuffs.Length > 0){
+            target.RemoveDebuffs(potion.HealDebuffs.Cast<HealingMoves.HealDebuff>().ToArray());
+            message += $" {targetName}'s  {string.Join(", ", potion.HealDebuffs)} back to normal.";
+        }
+
         //cure status, if status is matches the cure status
 
 
@@ -227,25 +275,37 @@ public class DamageCalculations : MonoBehaviour
     }
 
     public IEnumerator OnStatus(CharacterTemplate user, Moves move, CharacterTemplate target){
+
+        string userName = user.characterData.CharaStatList.CharacterName;
+        string targetName = target.characterData.CharaStatList.CharacterName;
+
         //mp loss and mp bar update
         user.TakeMP(CalcMPLoss(move));
 
         yield return StartCoroutine(uiManager.UpdateMP(user));
         uiManager.UpdateMPPanel(user);
 
+        string message = $"{userName} used {move.MoveName} on {targetName}.";
+
         //user anim plays
         //if move type is supplementary target is buffed or debuffed
         SupplementaryMoves supplementaryMove = (SupplementaryMoves)move;
         if(supplementaryMove.Debuffs.Length > 0){
             target.ApplyDebuff(supplementaryMove.Debuffs.Cast<DamagingMoves.Debuff>().ToArray(), supplementaryMove.DebuffValue);
+            message += $" {targetName}'s  {string.Join(", ", supplementaryMove.Debuffs)} lowered.";
         } else if(supplementaryMove.Buffs.Length > 0){
-            
+            target.ApplyBuff(supplementaryMove.Buffs, supplementaryMove.BuffValue);
+            message += $" {targetName}'s  {string.Join(", ", supplementaryMove.Buffs)} raised.";
         }
 
 
         //status applied to target
-        //cures debuffs if the move is a cure move
-        //cure status, if status is matches the cure status
+
+        Debug.Log(message);
+        generator.AddToBattleLog(message);
+
         //seconds pass
     }
+
+
 }
