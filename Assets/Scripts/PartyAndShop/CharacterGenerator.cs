219d8def587a338e67de4a7e19eb6121bc3abef2
Bami O. <bamideleogundeji949@gmail.com>
2024-08-28 15:09:49 -0400

    refactor: Update party generation logic in CharacterGenerator.cs

--- Assets/Scripts/PartyAndShop/CharacterGenerator.cs
+++ Assets/Scripts/PartyAndShop/CharacterGenerator.cs
@@ -9,6 +9,7 @@ public class CharacterGenerator : MonoBehaviour
     public GameObject invenCharacterPrefab;
     public Transform partyContainer;
     public Transform invenContainer;
+    public float buttonSpacing;
 
     PartyManager partyManager;
 
@@ -18,7 +19,30 @@ public class CharacterGenerator : MonoBehaviour
     }
     public void GenerateParty()
     {
+        //clear the party container before generating new party members
+        foreach (Transform partyCharacter in partyContainer)
+        {
+            Destroy(partyCharacter.gameObject);
+        }
+
+        float currentPosY = 100f;
+
         Debug.Log("Generating Party");
-        //generate all 4 party members from the party manager
+        //generate all 4 party members from the party manager to the party container using the party character prefab
+        foreach (CharacterTemplate character in partyManager.currentParty)
+        {
+            GameObject partyCharacter = Instantiate(partyCharacterPrefab, partyContainer);
+            //make space for the character in the party container using the currentPosY
+            RectTransform rect = partyCharacter.GetComponent<RectTransform>();
+            //give space between buttons so that the next button goes below the previous one
+            rect.anchoredPosition = new Vector2(0, currentPosY);
+            currentPosY -= buttonSpacing + rect.sizeDelta.y;
+
+            
+
+
+
+        }
+         
     }
 }
