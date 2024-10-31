31b39cb908acde8297388b22ef61fe2c48b7d308
Bami O. <bamideleogundeji949@gmail.com>
2024-10-31 13:27:27 -0400

    Add ItemGenerator script for dynamic item creation in PartyAndShop

---
+++ Assets/Scripts/PartyAndShop/Generator/ItemGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemGenerator : MonoBehaviour{
+    [SerializeField] GameObject itemContainer;
+
+    public void GeneratePotions(){}
+
+    public void GenerateWeapons(){}
+
+    public void GenerateEquips(){}
+
+    public void GenerateTools(){}
+
+    public void GenerateTreasures(){}
+
+    public void GenerateSpecial(){}
+
+    //tentative: grabbing the items needed from the inventory (currently on the canvas of unity)
+
+    public void GenerateItems(){
+        
+    }
+}
